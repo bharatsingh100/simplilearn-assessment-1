0f279a75a595604bd9db90fc2f6dc31730f8099a
unthinkable <bharatsinghkushwah1000@gmail.com>
2022-09-12 18:07:54 +0530

    file adding functionality created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Scanner;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class Main {
 
@@ -10,6 +16,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the command line application by Bharat Singh Kushwah");
 
+        File projectDir = new File("src/com/company/opFiles");
 	    boolean start = true;
 	    while(start){
 
@@ -18,37 +25,62 @@ public class Main {
                     "press 0");
             char n = sc.next().charAt(0);
 
-            if(n == '1') firstOption();
-            if(n == '2') secondOption(sc);
+            if(n == '1') firstOption(projectDir);
+            if(n == '2') secondOption(sc,projectDir);
             if(n == '0') start = false;
         }
 
     }
 
-    private static void firstOption(){
-        System.out.println("Files are");
-        File opFiles = new File("src/com/company/opFiles");
-
-        File files[] = opFiles.listFiles();
+    private static void firstOption(File projectDir){
+        File files[] = projectDir.listFiles();
         String fileNames[] = new String[files.length];
         for(int i=0; i<files.length; i++){
             fileNames[i] = files[i].getName();
         }
         Arrays.sort(fileNames);
 
+        System.out.println("Files are");
         for(int i=0; i<files.length; i++){
             System.out.println(fileNames[i]);
         }
     }
 
-    private static void secondOption(Scanner sc){
+    private static void secondOption(Scanner sc,File projectDir){
         boolean cont = true;
+
         while(cont){
-            System.out.println("To add new file press 1, to delete a file press 2, to search a file press 3" +
-                    "to get back to main menu press 0");
+            System.out.println("To add new file press 1, to delete a file press 2, \nto search a file" +
+                    " press 3 or to get back to main menu press 0");
             char c = sc.next().charAt(0);
-            if(c == '1');
+            if(c == '1'){
+                System.out.println("Enter the location of file you want to add to application directory");
+                String fileSource = sc.next();
+                System.out.println("Enter file name");
+                String filename = sc.next();
+                Path source = Paths.get(fileSource);
+                Path destFilePath = Paths.get(projectDir.getPath().toString() + "/" + filename);
+                try{
+                    Files.copy(source,destFilePath,REPLACE_EXISTING);
+                }
+                catch(IOException e){
+                    System.out.println(e.getMessage());
+                }
+
+            }
+            if(c == '2'){
+                System.out.println("doing nothing yet");
+            }
+            if(c == '3'){
+                System.out.println("im also doing nothing");
+            }
+            if(c == '0') break;
+
+            else{
+                System.out.println("invalid input try again");
+            }
         }
+
     }
 
 
